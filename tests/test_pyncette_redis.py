15dab00f01c26b377d75ca61f48ed6326c62162a
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2020-01-05 12:57:51 +0000

    Fix the formatting, add a best-effort integration test

--- tests/test_pyncette_redis.py
+++ tests/test_pyncette_redis.py
@@ -1,15 +1,15 @@
 import asyncio
 import datetime
 from unittest.mock import MagicMock
+from uuid import uuid4
 
 import pytest
 
-
-from uuid import uuid4
 from pyncette import Context
 from pyncette import ExecutionMode
 from pyncette import Pyncette
 from pyncette.repository.redis import redis_repository
+
 
 @pytest.mark.asyncio
 @pytest.mark.integration
@@ -29,6 +29,7 @@ async def test_successful_task_interval(monkeypatch):
 
     assert counter.call_count == 2
 
+
 @pytest.mark.asyncio
 @pytest.mark.integration
 async def test_failing_task_interval(monkeypatch):
@@ -47,3 +48,27 @@ async def test_failing_task_interval(monkeypatch):
     await task
 
     assert counter.call_count == 6
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_failing_task_interval_best_effort(monkeypatch):
+    app = Pyncette(redis_url="redis://localhost", repository_factory=redis_repository)
+
+    counter = MagicMock()
+
+    @app.task(
+        interval=datetime.timedelta(seconds=5),
+        name=str(uuid4()),
+        execution_mode=ExecutionMode.BEST_EFFORT,
+    )
+    async def successful_task(context: Context) -> None:
+        counter()
+        raise RuntimeError("Oops")
+
+    task = asyncio.create_task(app.run())
+    await asyncio.sleep(10.5)
+    app.shutdown()
+    await task
+
+    assert counter.call_count == 2
