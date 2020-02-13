018729406856cf004b1263d7396c677743eb509e
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2020-02-13 23:30:23 +0000

    Bump version: 0.2.2 → 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "Pyncette"
 year = "2019"
 author = "Tibor Djurica Potpara"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.2.2"
+version = release = "0.3.0"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="pyncette",
-    version="0.2.2",
+    version="0.3.0",
     license="MIT",
     description="A reliable distributed cron with pluggable storage backends",
     long_description="%s\n%s"
