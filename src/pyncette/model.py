385f67caae9a03e3f9b461493c9386d80fa61822
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2024-11-25 22:48:37 +0000

    chore: Upgrade dependencies + enable on Python 3.12 and 3.13 (#42)

--- src/pyncette/model.py
+++ src/pyncette/model.py
@@ -37,11 +37,9 @@ class Context:
 
     if TYPE_CHECKING:
 
-        def __getattr__(self, name: str) -> Any:
-            ...
+        def __getattr__(self, name: str) -> Any: ...
 
-        def __setattr__(self, name: str, value: Any) -> Any:
-            ...
+        def __setattr__(self, name: str, value: Any) -> Any: ...
 
 
 class TaskFunc(Protocol):
